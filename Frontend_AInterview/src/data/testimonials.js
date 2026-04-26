5fcb5b9ebf7820b7fe4264a0ba9f32690a47dc42
Ganesh-123-maker <ganeshbarmavath24@gmail.com>
2026-04-26 12:03:08 +0530

    Add testimonials data folder

---
+++ Frontend_AInterview/src/data/testimonials.js
@@ -0,0 +1,20 @@
+export const testimonials = [
+  {
+    name: "Abhishek Yadav",
+    role: "ML Engineer @ Amazon",
+    feedback:
+      "InterviewMind AI helped me prepare for system design and coding rounds. The mock interviews were incredibly realistic!",
+  },
+  {
+    name: "Barmavath Ganesh",
+    role: "Frontend Developer @ Google",
+    feedback:
+      "The AI feedback was amazing. It pointed out exactly where I needed to improve in my answers.",
+  },
+  {
+    name: "Animesh Khare",
+    role: "Backend Engineer @ Microsoft",
+    feedback:
+      "Practicing interviews regularly on this platform boosted my confidence a lot.",
+  }
+];
