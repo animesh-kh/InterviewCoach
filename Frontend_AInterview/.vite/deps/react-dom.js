aaeebe4962136e85dd92e15033002671e92769fa
Ganesh-123-maker <ganeshbarmavath24@gmail.com>
2026-04-26 12:07:15 +0530

    Remove .vite from repo

--- Frontend_AInterview/.vite/deps/react-dom.js
+++
@@ -1,6 +0,0 @@
-import {
-  require_react_dom
-} from "./chunk-Q4IEAB75.js";
-import "./chunk-PC7AKF3Z.js";
-import "./chunk-2TUXWMP5.js";
-export default require_react_dom();
--- Frontend_AInterview/.vite/deps/react_jsx-runtime.js
+++
@@ -1,6 +0,0 @@
-import {
-  require_jsx_runtime
-} from "./chunk-BMSVSISV.js";
-import "./chunk-PC7AKF3Z.js";
-import "./chunk-2TUXWMP5.js";
-export default require_jsx_runtime();
